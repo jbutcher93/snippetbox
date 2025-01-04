b760f3c004a93bf4242c1b82ca6ce0d9e267c5fc
jbutcher93 <1butcherjohn@gmail.com>
2025-01-03 21:00:27 -0500

    11.4 WIP

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -150,6 +150,17 @@ func (app *application) userSignupPost(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "/user/login", http.StatusSeeOther)
 }
 
-func (app *application) userLogin(w http.ResponseWriter, r *http.Request)      {}
+type userLoginForm struct {
+	Email               string `form:"email"`
+	Password            string `form:"password"`
+	validator.Validator `form:"-"`
+}
+
+func (app *application) userLogin(w http.ResponseWriter, r *http.Request) {
+	data := app.newTemplateData(r)
+	data.Form = userLoginForm{}
+	app.render(w, http.StatusOK, "login.tmpl", data)
+}
+
 func (app *application) userLoginPost(w http.ResponseWriter, r *http.Request)  {}
 func (app *application) userLogoutPost(w http.ResponseWriter, r *http.Request) {}
